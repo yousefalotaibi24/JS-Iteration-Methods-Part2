300cfea219f6781d9d8a28a067aba6c81804347a
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-06 17:19:59 +0300

    modified

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const transactions = [
   ["expense", 300],
   ["income", 700],
 ];
+// Part 1 Transaction Analysis
+
+//const income = transactions.filter((["income", 1000]) => ["income", 1000][0] == "income");
+
+// Task 1
+const income = transactions.filter((transaction) => transaction[0] == "income");
+
+console.log(income);
+// Task2
+const expense = transactions.filter(
+  (transaction) => transaction[0] == "expense"
+);
+console.log(expense);
+
+// Part 2 Financial Summary
